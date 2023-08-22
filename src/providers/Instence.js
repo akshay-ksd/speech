1c5fd166a1563aff34d5fbde7a63762f0bd7e907
Akshay k <56345666+akshay-ksd@users.noreply.github.com>
2023-08-22 11:06:59 +0530

    Update Instence.js

--- src/providers/Instence.js
+++ src/providers/Instence.js
@@ -4,7 +4,7 @@ const Instance = axios.create({
   baseURL: 'https://api.openai.com/v1',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': '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',
+    'Authorization': 'api-key',
   },
 });
 
